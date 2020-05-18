8ee9d19c41ede7b658db62291aff73c4dfb2c5c2
Gabriel <gabrielviscondi@gmail.com>
2020-05-17 21:48:28 -0300

    adding generic page

--- recipeblog/blog/views.py
+++ recipeblog/blog/views.py
@@ -5,3 +5,7 @@ from .models import Receita
 def post_list(request):
     receitas = Receita.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'receitas': receitas})
+
+def generic(request):
+    receita = Receita.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/generic.html', {'receita': receita[0]})
